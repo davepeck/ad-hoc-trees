f2dd570d690b2ec554e903f0bdafec708fb0e6fb
Dave <davepeck@gmail.com>
2022-05-15 16:49:42 -0700

    Even more drawing nonsense

--- src/drawTree.ts
+++ src/drawTree.ts
@@ -21,6 +21,9 @@ export interface TreeOptions {
   /** Just how curvy should things be? */
   curviness: number;
 
+  /** How likely is it that we have extra branches? */
+  density: number;
+
   /** A random number seed. */
   seed: number;
 
@@ -36,6 +39,7 @@ export const DEFAULT_TREE_OPTIONS: TreeOptions = {
   spread: 0.325,
   lengthVariation: 0.25,
   curviness: 0.2,
+  density: 0.03,
   seed: 1,
   randomness: 1.0,
 };
@@ -48,6 +52,7 @@ export const ELLIE_DEFAULT_TREE_OPTIONS: TreeOptions = {
   spread: 0.225,
   lengthVariation: 1.3,
   curviness: 0,
+  density: 0.03,
   seed: 1,
   randomness: 0,
 };
@@ -60,6 +65,7 @@ export const TEST_DEFAULT_TREE_OPTIONS: TreeOptions = {
   spread: 0.675,
   lengthVariation: 4,
   curviness: 0.2,
+  density: 0.03,
   seed: 1,
   randomness: 1.0,
 };
@@ -114,8 +120,14 @@ export const DRAW_TREE_RANGES: Record<keyof TreeOptions, OptionRange> = {
   curviness: {
     label: "Curviness",
     min: 0,
-    max: 1,
+    max: Math.PI,
     step: 0.01,
+  },
+  density: {
+    label: "Density",
+    min: 0,
+    max: 0.15,
+    step: 0.0001,
   },
   seed: {
     label: "Seed",
@@ -128,9 +140,8 @@ export const DRAW_TREE_RANGES: Record<keyof TreeOptions, OptionRange> = {
     min: 0,
     max: 1,
     step: 0.1,
-  }
+  },
 };
-
 
 /** Generate a random leaf color! */
 const leafColor = (options: TreeOptions, random: SimpleRandom): string => {
@@ -147,7 +158,7 @@ const leafColor = (options: TreeOptions, random: SimpleRandom): string => {
   const l = linear(defaultL, randomL, options.randomness);
   const a = linear(defaultA, randomA, options.randomness);
   return `hsla(${h}, ${s}%, ${l}%, ${a})`;
-}
+};
 
 /** Draw a single leaf! */
 const drawLeaf = (
@@ -165,10 +176,27 @@ const drawLeaf = (
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.fill();
   ctx.closePath();
-}
+};
 
 /** Generate a linear interplation between two points. */
 const linear = (a: number, b: number, t: number) => a * (1 - t) + b * t;
+
+/** Draw a linear branch segment. */
+const drawBranchSegment = (
+  ctx: CanvasRenderingContext2D,
+  from: { x: number; y: number },
+  to: { x: number; y: number },
+  width: number
+) => {
+  ctx.beginPath();
+  ctx.strokeStyle = "#3c3c3c";
+  ctx.lineCap = "round";
+  ctx.lineWidth = width;
+  ctx.moveTo(from.x, from.y);
+  ctx.lineTo(to.x, to.y);
+  ctx.stroke();
+  ctx.closePath();
+};
 
 /** Draw a curvy branch. */
 const drawBranch = (
@@ -179,22 +207,65 @@ const drawBranch = (
   y: number,
   options: TreeOptions,
   random: SimpleRandom
-): { x: number, y: number } => {
+): { x: number; y: number } => {
+  const BRANCH_SEGMENT_LENGTH = 10;
+
   const depthPercent = depth / options.depth;
   const typicalLength = options.length * depthPercent;
-  const randomLength = typicalLength * random.normal(1 - options.lengthVariation, 1 + options.lengthVariation);
+  const randomLength =
+    typicalLength *
+    random.uniform(1 - options.lengthVariation, 1 + options.lengthVariation);
   const length = linear(typicalLength, randomLength, options.randomness);
-  const top = circlePoint(x, y, length, angle);
-  ctx.beginPath();
-  ctx.strokeStyle = "#3c3c3c";
-  ctx.lineCap = "round";
-  ctx.lineWidth = options.width * depthPercent;
-  ctx.moveTo(x, y);
-  ctx.lineTo(top.x, top.y);
-  ctx.stroke();
-  ctx.closePath();
-  return top;
-}
+
+  const segments = Math.round(length / BRANCH_SEGMENT_LENGTH);
+  const leftoverSegmentLength = length - segments * BRANCH_SEGMENT_LENGTH;
+  const defaultCurveDirection = (angle > Math.PI / 2) ? 1 : -1;
+  const randomCurveDirection = (random.uniform(0, 1) < 0.5) ? -1 : 1;
+  const curveDirection = linear(defaultCurveDirection, randomCurveDirection, options.randomness);
+  const curveAngleIncrement = options.curviness * curveDirection * (1.0 - depthPercent);
+  let current = { x, y };
+  let curveAngle = angle;
+  ctx.moveTo(current.x, current.y);
+  for (let i = 0; i < segments; i++) {
+    const segmentTop = circlePoint(
+      current.x,
+      current.y,
+      BRANCH_SEGMENT_LENGTH,
+      curveAngle
+    );
+    drawBranchSegment(ctx, current, segmentTop, options.width * depthPercent);
+
+    const branchRandom = linear(0, random.uniform(0, 1), options.randomness);
+    if (branchRandom > (1.0 - options.density)) {
+      const randomDirection = random.uniform(0, 1) < 0.5 ? -1 : 1;
+      drawTreeRecursive(
+        ctx,
+        depth - 1,
+        curveAngle + randomDirection * options.spread,
+        segmentTop.x,
+        segmentTop.y,
+        options,
+        random
+      )
+    }
+
+    current = segmentTop;
+    curveAngle += curveAngleIncrement;
+  }
+
+  if (leftoverSegmentLength > 0) {
+    const segmentTop = circlePoint(
+      current.x,
+      current.y,
+      leftoverSegmentLength,
+      curveAngle
+    );
+    drawBranchSegment(ctx, current, segmentTop, options.width * depthPercent);
+    current = segmentTop;
+  }
+
+  return current;
+};
 
 /** Draw a branch of our tree, recursively! */
 const drawTreeRecursive = (
@@ -204,15 +275,35 @@ const drawTreeRecursive = (
   x: number,
   y: number,
   options: TreeOptions,
-  random: SimpleRandom,
+  random: SimpleRandom
 ) => {
   if (depth > 0) {
     const top = drawBranch(ctx, depth, angle, x, y, options, random);
 
     // draw our left and right branches
-    const deviation = linear(options.spread, options.spread * random.uniform(0.5, 1.5), options.randomness);
-    drawTreeRecursive(ctx, depth - 1, angle - deviation, top.x, top.y, options, random);
-    drawTreeRecursive(ctx, depth - 1, angle + deviation, top.x, top.y, options, random);
+    const deviation = linear(
+      options.spread,
+      options.spread * random.uniform(0.5, 1.5),
+      options.randomness
+    );
+    drawTreeRecursive(
+      ctx,
+      depth - 1,
+      angle - deviation,
+      top.x,
+      top.y,
+      options,
+      random
+    );
+    drawTreeRecursive(
+      ctx,
+      depth - 1,
+      angle + deviation,
+      top.x,
+      top.y,
+      options,
+      random
+    );
 
     // if we're near the top, draw a leaf
     if (depth <= 2) {
