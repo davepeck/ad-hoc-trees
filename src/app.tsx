5de970d18a70116c9b2782ea58b0db4d360fa028
Dave <davepeck@gmail.com>
2022-05-15 20:48:19 -0700

    Clean it up.

--- src/app.tsx
+++ src/app.tsx
@@ -15,45 +15,63 @@ import {
   TREE_RANGE_SETTINGS as SIMPLE_TREE_RANGE_SETTINGS,
 } from "./tree/simple";
 
+/** Our simple tree component. */
+const SimpleTree = () => {
+  const [options, setOptions] =
+    useState<SimpleTreeOptions>(ELLIES_TREE_OPTIONS);
+
+  return (
+    <>
+      <Ranges
+        onChange={(name, value) => setOptions({ ...options, [name]: value })}
+        settings={SIMPLE_TREE_RANGE_SETTINGS}
+        initials={options}
+      />
+      <div className="tree-container">
+        <Canvas
+          draw={(ctx) => simpleDrawTree(ctx, options as any)}
+          className="tree-drawing"
+        />
+      </div>
+    </>
+  );
+};
+
+/** Our fancy tree component. */
+const FancyTree = () => {
+  const [options, setOptions] = useState<FancyTreeOptions>(DAVES_TREE_OPTIONS);
+
+  return (
+    <>
+      <Ranges
+        onChange={(name, value) => setOptions({ ...options, [name]: value })}
+        settings={FANCY_TREE_RANGE_SETTINGS}
+        initials={options}
+      />
+      <div className="tree-container">
+        <Canvas
+          draw={(ctx) => fancyDrawTree(ctx, options as any)}
+          className="tree-drawing"
+        />
+      </div>
+    </>
+  );
+};
+
 /** Our primary application. */
 export const App: React.FC = () => {
   const [mode, setMode] = useState<"simple" | "fancy">("simple");
-  const [options, setOptions] = useState<SimpleTreeOptions | FancyTreeOptions>(ELLIES_TREE_OPTIONS);
-
-  const updateMode = (newMode: "simple" | "fancy") => {
-    setMode(newMode);
-    if (newMode === "simple") {
-      setOptions(ELLIES_TREE_OPTIONS);
-    }
-    else {
-      setOptions(DAVES_TREE_OPTIONS);
-    }
-  };
-
-  const drawTree = mode === "simple" ? simpleDrawTree : fancyDrawTree;
-  const rangeSettings = mode === "simple" ? SIMPLE_TREE_RANGE_SETTINGS : FANCY_TREE_RANGE_SETTINGS;
 
   return (
     <div className="outer">
       <h1>Trees!</h1>
       <h3>Project #1, 2022-05-15</h3>
-      <Segmented segments={{ "simple": "Simple", "fancy": "Fancy" }} initialSelected={mode} onChange={(id) => updateMode(id as "simple" | "fancy")} />
-
-      {/* <select onChange={(e) => updateMode(e.target.value as "simple" | "fancy")}>
-        <option value="simple" selected={mode === "simple"}>Simple</option>
-        <option value="fancy" selected={mode === "fancy"}>Fancy</option>
-      </select> */}
-      <Ranges
-        onChange={(name, value) => setOptions({ ...options, [name]: value })}
-        settings={rangeSettings}
-        initials={options}
+      <Segmented
+        segments={{ simple: "Simple", fancy: "Fancy" }}
+        initialSelected={mode}
+        onChange={(id) => setMode(id as "simple" | "fancy")}
       />
-      <div className="tree-container">
-        <Canvas
-          draw={(ctx) => drawTree(ctx, options as any)}
-          className="tree-drawing"
-        />
-      </div>
+      {mode === "simple" ? <SimpleTree /> : <FancyTree />}
     </div>
   );
 };
