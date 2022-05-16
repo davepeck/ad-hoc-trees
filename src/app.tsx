db38061bc0632681280aef66d8ff2b7e7fab37ab
Dave <davepeck@gmail.com>
2022-05-15 19:39:07 -0700

    Simple or complex, almost

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { Canvas } from "./Canvas";
 import { Ranges } from "./Ranges";
-// import {
-//   drawTree,
-//   TreeOptions,
-//   DAVES_TREE_OPTIONS,
-//   TREE_RANGE_SETTINGS,
-// } from "./tree/fancy";
 import {
-  drawTree,
-  TreeOptions,
+  drawTree as fancyDrawTree,
+  TreeOptions as FancyTreeOptions,
+  DAVES_TREE_OPTIONS,
+  TREE_RANGE_SETTINGS as FANCY_TREE_RANGE_SETTINGS,
+} from "./tree/fancy";
+import {
+  drawTree as simpleDrawTree,
+  TreeOptions as SimpleTreeOptions,
   ELLIES_TREE_OPTIONS,
-  TREE_RANGE_SETTINGS,
+  TREE_RANGE_SETTINGS as SIMPLE_TREE_RANGE_SETTINGS,
 } from "./tree/simple";
 
 /** Our primary application. */
 export const App: React.FC = () => {
-  const [options, setOptions] = useState<TreeOptions>(ELLIES_TREE_OPTIONS);
+  const [options, setOptions] = useState<SimpleTreeOptions | FancyTreeOptions>(DAVES_TREE_OPTIONS);
 
   return (
     <div className="outer">
@@ -24,12 +24,12 @@ export const App: React.FC = () => {
       <h3>Project #1, 2022-05-15</h3>
       <Ranges
         onChange={(name, value) => setOptions({ ...options, [name]: value })}
-        settings={TREE_RANGE_SETTINGS}
+        settings={FANCY_TREE_RANGE_SETTINGS}
         initials={options}
       />
       <div className="tree-container">
         <Canvas
-          draw={(ctx) => drawTree(ctx, options)}
+          draw={(ctx) => fancyDrawTree(ctx, options as any)}
           className="tree-drawing"
         />
       </div>
